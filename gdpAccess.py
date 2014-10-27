a70caec8b9605de706827e3425d4c75789680908
Alex Tyler <alexanderhtyler@gmail.com>
2014-10-27 13:24:21 -0700

    run multiple scripts

--- gdpAccess.py
+++ gdpAccess.py
@@ -6,45 +6,48 @@ import urllib2
 import json
 from subprocess import call
 
-class GDP:
-    baseURI = "http://localhost:8080/gdp/v1/gcl"
-    gcl_name = ""
+class GCL:
+    baseURI = "http://localhost:8008/gdp/v1/gcl"
     gdpPath = ""
+    gcl_name = ""
 
-    def __init__(self, gcl_name, gdpPath):
+    def __init__(self, gcl_name):
         self.gcl_name = gcl_name
-        self.gdpPath = gdpPath
-        #self.baseURI = "http://localhost:8080/gdp/v1/gcl"
 
-    def __init__(self, gdpPath):
-        self.gdpPath = gdpPath
-
+    """def __init__(self, gcl_name, baseURI):
+        self.gcl_name = gcl_name
+        self.baseURI = baseURI
+"""
     #only need this while running from a local machine
     def startServer(self):
         call(['/home/alex/Cs/Swarm/ble/startGDP'])
 
-    #sets up a shell for using writer-test
+    #sets up a shell for using writer-test, not needed
     #returns a pexpect object
     def writerTest(self):
         gdpCon = pexpect.spawn(self.gdpPath + 'apps/writer-test ' + self.devName)
         return gdpCon
 
-    def initGCL(self):
-        if (self.gcl_name is not ""):
-            print "GCL is already initialized."
-            return
+    def getGCL(self):
+        #if (self.gcl_name is not ""):
+            #print "GCL is already initialized."
+            #return
         req = urllib2.Request(self.baseURI)
-        #req.add_header('Content-Type', 'application/json')
+        req.add_header('Content-Type', 'application/json')
         response = urllib2.urlopen(req, "")
         returnJson = response.read()
         decoded = json.loads(returnJson)
-        print "GCL_name is set to " + decoded["gcl_name"]
-        self.gcl_name = decoded["gcl_name"]
+        print "GCL_name is " + decoded["gcl_name"]
+        #self.gcl_name = decoded["gcl_name"]
+        return decoded["gcl_name"]
+
 
     #value must be a string
     def gdpPOST(self, value):
+        print "Posting"
         url = self.baseURI + "/" + self.gcl_name
         data = { "value" : value }
+        print url
         req = urllib2.Request(url)
         req.add_header('Content-Type', 'application/json')
         response = urllib2.urlopen(req, json.dumps(data))
